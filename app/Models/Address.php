11d4c743a149e32ffc743e49f19e8db5d077eb1c
vladimir <svestudy@rambler.ru>
2026-06-15 00:31:19 +0300

    Add: Address model

---
+++ app/Models/Address.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Address extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'name',
+        'phone',
+        'street',
+        'city',
+        'state',
+        'postal_code',
+        'country',
+        'is_default',
+    ];
+
+    protected $casts = [
+        'is_default' => 'boolean',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function getFullAddressAttribute()
+    {
+        return "{$this->street}, {$this->city}, {$this->state} {$this->postal_code}, {$this->country}";
+    }
+}
